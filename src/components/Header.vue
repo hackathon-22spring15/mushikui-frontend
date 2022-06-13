6d2932a3aaba07cf17914c64daab7cf4d8b7e067
trasta <trasta298@gmail.com>
2022-06-14 00:31:22 +0900

    ⚡️ HeaderとHomeに分割

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+    },
+})
+</script>
+
+<template>
+    <div>
+        Header
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+    },
+})
+</script>
+
+<template>
+    <div>
+        Home
+    </div>
+</template>
+
+<style scoped>
+
+</style>

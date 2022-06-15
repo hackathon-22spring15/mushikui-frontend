0e0e49e63db462cc79e0ebe516e5971d66701879
Yuchi Yamaguchi <abap0002@gmail.com>
2022-06-15 15:09:26 +0900

    キーボードの各キーのコンポーネントを作成

---
+++ src/components/Key.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+export default {
+    name: "Key",
+    props: ["char", "input"]
+}
+
+
+
+
+
+
+</script>
+
+<template>
+    <button @click="input(char)">{{ char }}</button>
+</template>
+
+
+<style scoped>
+button{
+    border: none;
+    border-radius: 10%;
+    background-color: rgb(241, 241, 241);
+    font-size: 30px;
+    padding: 10px;
+    text-align: center;
+    margin: 1px;
+}
+</style>

49a652dd0fe9832db3eff303a165dbbb4c4d85ff
trasta <trasta298@gmail.com>
2022-06-16 18:26:14 +0900

    🐛 fix

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-restricted-imports
 import { Apis, Configuration } from './generated'
 
-const apis = new Apis(new Configuration({ basePath: '/' }))
+const apis = new Apis(new Configuration())
 
 export default apis
 // eslint-disable-next-line no-restricted-imports

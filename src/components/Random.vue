73a0ac306de4faceda4365ead6e508d3ebeb5982
20jun01 <10junjun20@gmail.com>
2022-06-18 21:12:29 +0900

    random実装

--- src/components/Random.vue
+++ src/components/Random.vue
@@ -5,6 +5,7 @@ import ResultModal from "./Modal.vue";
 import apis, { Expression } from "../lib/apis";
 import { transSymbol } from "../utils";
 import { useToast } from "vue-toastification";
+import { useRoute, useRouter } from 'vue-router'
 
 const check_finished = (row: Array<number>) => {
   return row.every((e) => e === 2);
@@ -31,9 +32,13 @@ export default defineComponent({
     const can_input = ref(true);
     const seed = ref(0);
     const toast = useToast();
+    const route = useRoute();
+    // const { seeds } = route.params;
+    // seed.value = Number(seeds);
 
     onBeforeMount(async () => {
-      seed.value = Math.floor(Math.random() * 100000);
+      const { seeds } = route.params
+      seed.value = Number(seeds)
       try {
         can_input.value = false;
         const { data } = await apis.getEqualRandomExpressionRandomSeedGet(
